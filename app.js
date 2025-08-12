bd4161e3f2951ca010d284b45148c5b04e25eb85
Ikejiri-1 <pedroike21@gmail.com>
2025-08-12 12:34:35 -0300

    cors

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 import express from "express";
 import router from "./rotas/index.rotas.js";
+import cors from "cors";
+
 const app = express();
 
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(router);
 
